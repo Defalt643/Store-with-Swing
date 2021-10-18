fa4d0a7e7bbd6ef96ccc45324798e1eb5a3d169b
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-10-18 19:11:21 +0700

    create Receipt and ReceiptDetail classes

---
+++ src/main/java/model/Receipt.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author ming
+ */
+public class Receipt {
+
+    private int id;
+    private Date created;
+    private User seller;
+    private Customer customer;
+    private ArrayList<ReceiptDetail> receiptDetail;
+
+    public Receipt(int id, Date created, User seller, Customer customer) {
+        this.id = id;
+        this.created = created;
+        this.seller = seller;
+        this.customer = customer;
+        receiptDetail = new ArrayList<>();
+    }
+
+    public Receipt(User seller, Customer customer) {
+        this(-1, null, seller, customer);
+    }
+
+    public void addReceiptDetail(int id, Product product, int amount, double price) {
+        receiptDetail.add(new ReceiptDetail(id, product, amount, price, this));
+    }
+
+    public void addReceiptDetail(Product product, int amount, double price) {
+        addReceiptDetail(-1, product, amount, price);
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (ReceiptDetail r : receiptDetail) {
+            total = total + r.getTotal();
+        }
+        return total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public ArrayList<ReceiptDetail> getReceiptDetail() {
+        return receiptDetail;
+    }
+
+    public void setReceiptDetail(ArrayList<ReceiptDetail> receiptDetail) {
+        this.receiptDetail = receiptDetail;
+    }
+
+}
---
+++ src/main/java/model/ReceiptDetail.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ming
+ */
+public class ReceiptDetail {
+
+    private int id;
+    private Product product;
+    private int amount;
+    private double price;
+    private Receipt receipt;
+
+    public ReceiptDetail(int id, Product product, int amount, double price, Receipt receipt) {
+        this.id = id;
+        this.product = product;
+        this.amount = amount;
+        this.price = price;
+        this.receipt = receipt;
+    }
+
+    public ReceiptDetail(Product product, int amount, double price, Receipt receipt) {
+        this(-1, product, amount, price, receipt);
+
+    }
+
+    public double getTotal() {
+        return amount * price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Receipt getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(Receipt receipt) {
+        this.receipt = receipt;
+    }
+
+}
